d9c8a141ccc810eebbdf7e345d07ec735c2d27fa
Marina Poselentseva <marja555@yandex.ru>
2022-02-28 16:50:33 +0500

    Отобразить карточки

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function ImagePopup() {
+function ImagePopup(props) {
   return (
     <div className="popup popup_type_pic">
     <div className="popup__overlay"></div>
     <div className="popup__pic-container">
-      <img src="#" alt="Изображение." className="popup__pic" />
-      <h2 className="popup__pic-title">картинка</h2>
+      <img src={props.card.link} alt="Изображение." className="popup__pic" />
+      <h2 className="popup__pic-title">props.card.name</h2>
       <button type="button" className="popup__close"></button>
     </div>
   </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from '../utils/api';
+import Card from './Card';
 
 function Main(props) {
+
+  const [userName, setUserName] = useState('');
+  const [userDescription, setUserDescription] = useState('');
+  const [userAvatar, setUserAvatar] = useState('');
+
+  const [cards, setCards] = useState([]);
+
+  React.useEffect(() => {
+    api.getUser()
+    .then((userData) => {
+      setUserName(userData.name);
+      setUserDescription(userData.about);
+      setUserAvatar(userData.avatar);
+    })
+    .catch(err => `Не удалось получить данные пользователя, ошибка: ${err}`)
+
+    api.getCards()
+    .then((cardsList) => {
+      setCards(cardsList);
+      console.log(cards)
+    })
+    .catch(err => `Не удалось получить данные карточек, ошибка: ${err}`)
+  }, [])
 
   return (
     <main>
       <section className="profile">
         <div className="profile__info">
-          <div onClick={props.onEditAvatar} className="profile__image"></div>
+          <div onClick={props.onEditAvatar} 
+          className="profile__image"
+          style={{ backgroundImage: `url(${userAvatar})` }}></div>
           <div className="profile__info-container">
-            <h1 className="profile__name"></h1>
+            <h1 className="profile__name">{userName}</h1>
             <button type="button" onClick={props.onEditProfile} className="profile__edit-button"></button>
-            <p className="profile__profession"></p>
+            <p className="profile__profession">{userDescription}</p>
           </div>
         </div>  
         <button type="button" onClick={props.onAddPlace} className="profile__add-button"></button>
       </section>
       <section className="cards">
-        <template id="cardTemplate">
-          <div className="card">
-            <img src="#" className="card__image" alt="" />
-            <button type="button" className="card__del-button"></button>
-            <div className="card__rectangle">
-              <h2 className="card__title"></h2>
-              <button type="button" className="card__like"></button>
-              <div className="card__likes-counter"></div>
-            </div>
-          </div>
-        </template>
+        {cards.map((cardData) => (
+          <Card 
+            card={cardData}
+          />
+        ))}
       </section>
     </main>
   );
