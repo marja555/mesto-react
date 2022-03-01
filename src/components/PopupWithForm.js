1fab803bdd485e6b8cb8757fe534195a16475c9b
Marina Poselentseva <marja555@yandex.ru>
2022-03-01 15:29:31 +0500

    Исправить ошиббку в форме

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -11,7 +11,7 @@ function PopupWithForm(props) {
     <div className="popup__container">
       <button type="button" onClick={props.onClose} className="popup__close"></button>
       <h3 className="popup__title">{props.title}</h3>
-      <form name={`${props.name}-form`} className="popup__form popup__form_type_profile" noValidate>
+      <form name={`${props.name}-form`} className={`popup__form popup__form_type_${props.name}`} noValidate>
         {props.children}
       </form>
     </div>
